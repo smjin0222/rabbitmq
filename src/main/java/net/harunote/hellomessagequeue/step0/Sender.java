5d20e56c8058c71e541f55a1fbdd5003bc154b0e
villainscode <villainscode@gmail.com>
2024-11-22 18:13:33 +0900

    step 0 추가

---
+++ src/main/java/net/harunote/hellomessagequeue/step0/Sender.java
@@ -0,0 +1,19 @@
+package net.harunote.hellomessagequeue.step0;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public Sender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
+        System.out.println("[#] Sent : " + message);
+    }
+}

3fb8d9e54d8cc1a352d2d94f9642e4868f893a32
villainscode <villainscode@gmail.com>
2024-12-21 14:44:26 +0900

    step4 pub/sub 모델을 이용한 뉴스 구독 예제

--- src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
+++ src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
@@ -3,6 +3,13 @@ package net.harunote.hellomessagequeue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Step 4 Subscribing to news using the Pub/Sub model (FANOUT_EXCHANGE)
+ *
+ * @author : codevillain
+ * @fileName : HelloMessageQueueApplication
+ * @since : 12/20/24
+ */
 @SpringBootApplication
 public class HelloMessageQueueApplication {
 

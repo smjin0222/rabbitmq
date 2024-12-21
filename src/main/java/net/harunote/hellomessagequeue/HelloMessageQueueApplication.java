7d19c9191d2465c17bb4be5485d7cc69491c03e1
villainscode <villainscode@gmail.com>
2024-12-21 14:48:34 +0900

    step4 pub/sub 모델을 이용한 뉴스 구독 예제

--- src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
+++ src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
@@ -3,6 +3,14 @@ package net.harunote.hellomessagequeue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Step 3 Example of implementing real-time notifications using the Pub/Sub model (using WebSocket and STOMP)
+ *
+ * @author : codevillain
+ * @fileName : HelloMessageQueueApplication
+ * @since : 12/10/24
+ */
+
 @SpringBootApplication
 public class HelloMessageQueueApplication {
 

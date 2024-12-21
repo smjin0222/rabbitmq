a705c9b8ad084ff15bf09063c65b78d339f408df
villainscode <villainscode@gmail.com>
2024-12-21 14:55:28 +0900

    step2 경쟁 소비자 패턴을 이용한 작업 분배 큐

--- src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
+++ src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
@@ -3,6 +3,15 @@ package net.harunote.hellomessagequeue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Step 2 Example of work distribution through competing consumer pattern
+ *
+ * @author : codevillain
+ * @fileName : HelloMessageQueueApplication
+ * @since : 12/5/24
+ */
+
 @SpringBootApplication
 public class HelloMessageQueueApplication {
 

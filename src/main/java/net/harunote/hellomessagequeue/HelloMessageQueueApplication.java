e3da24c2a7be6c6a5fa4b4556131ade01ef3c104
villainscode <villainscode@gmail.com>
2024-12-21 14:53:31 +0900

    step1 단순 메시지 전달 예제

--- src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
+++ src/main/java/net/harunote/hellomessagequeue/HelloMessageQueueApplication.java
@@ -3,6 +3,14 @@ package net.harunote.hellomessagequeue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Step 1 Example of Simple message send(Producer to Consumer)
+ *
+ * @author : codevillain
+ * @fileName : HelloMessageQueueApplication
+ * @since : 12/1/24
+ */
 @SpringBootApplication
 public class HelloMessageQueueApplication {
 
